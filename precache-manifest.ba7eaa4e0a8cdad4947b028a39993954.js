846820d51e4fe19873700fcafa4a4a3f6a6ebe96
bossman-codex <56489079+bossman-codex@users.noreply.github.com>
2022-02-03 20:16:14 +0100

    Updates

---
+++ precache-manifest.ba7eaa4e0a8cdad4947b028a39993954.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394a70f9005c75ff18e287dd369839f1",
+    "url": "/memeGenerator/index.html"
+  },
+  {
+    "revision": "3ba00a240b23b6e88a29",
+    "url": "/memeGenerator/static/js/2.fa550cd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memeGenerator/static/js/2.fa550cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6aba03daa20ad4e9cf",
+    "url": "/memeGenerator/static/js/main.4eb47de2.chunk.js"
+  },
+  {
+    "revision": "01d1e9c45111eb6d1397",
+    "url": "/memeGenerator/static/js/runtime-main.c1650455.js"
+  }
+]);
